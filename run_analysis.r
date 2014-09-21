fe44c7efd8b99d286fbe3eeb7fd998c04587d7bf
admirableghost <prashanthv.aiesec@gmail.com>
2014-09-21 14:36:05 +0530

    Update run_analysis.r

--- run_analysis.r
+++ run_analysis.r
@@ -9,17 +9,18 @@
 
 # The code:
 
-# 1. 
-subtest <- read.table("./UCIHAR/subject_test.txt")
-xtest <- read.table("./UCIHAR/X_test.txt")
-ytest <- read.table("./UCIHAR/Y_test.txt")
-subtrain <- read.table("./UCIHAR/subject_train.txt")
-xtrain <- read.table("./UCIHAR/X_train.txt")
-ytrain <- read.table("./UCIHAR/Y_train.txt")
-features <- read.table("./UCIHAR/features.txt")
-activities <- read.table("./UCIHAR/activity_labels.txt")
+# 1. I moved all these files individuallly to my working directory first
 
-# 2. 
+subtest <- read.table("subject_test.txt")
+xtest <- read.table("X_test.txt")
+ytest <- read.table("Y_test.txt")
+subtrain <- read.table("subject_train.txt")
+xtrain <- read.table("X_train.txt")
+ytrain <- read.table("Y_train.txt")
+features <- read.table("features.txt")
+activities <- read.table("activity_labels.txt")
+
+# 2. joining tables
 xall <- rbind(xtrain, xtest)
 yall <- rbind(ytrain, ytest)
 suball <- rbind(subtrain, subtest)
@@ -27,12 +28,12 @@ allofit <- cbind(suball, yall, xall)
 
 rm(xtest,ytest,xtrain,ytrain,subtrain,subtest,xall,yall,suball)  # housecleaning
 
-# 3. 
+# 3. new col
 featureNames <- as.character(features[,2])
 newCols <- c("subject", "activity", featureNames)
 colnames(allofit) <- newCols
 
-# 4. 
+# 4. necessary dimensions
 
 onlyMeans <- grep("mean()", colnames(allofit))
 onlyStDevs <- grep("std()", colnames(allofit))
@@ -55,7 +56,7 @@ for (i in 1:30) {
         
 }
 
-# 6. 
+# 6. final steps
 colnames(tidyframe)<-colnames(newDataFrame2) #rename the columns again, as the names get lost in the mix above
 levels(tidyframe[,2])<-c('walk','upstairswalk','downstairswalk', 'sit','stand', 'lay')
 write.table(tidyframe, "Samsung_Data.txt", sep = "")
